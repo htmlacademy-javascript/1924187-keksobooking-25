44bc45d1faa774e1958d604898f891b9d3d3ed59
Keks <keks@htmlacademy.ru>
2022-03-14 23:45:08 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,18 @@
+import {createSimilarAdvertisement} from './data.js';
+
+createSimilarAdvertisement();
+
+
+import {appendSimilarAdvertisement} from './card.js';
+
+appendSimilarAdvertisement(createSimilarAdvertisement());
+
+import {addDisabledStatus} from './form-status.js';
+
+addDisabledStatus();
+
+import {addActiveStatus} from './form-status.js';
+
+addActiveStatus();
+
+import './user-form.js';
